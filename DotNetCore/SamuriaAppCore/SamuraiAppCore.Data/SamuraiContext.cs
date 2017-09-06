625b810a0840a570c730c4a9fecfbe1aa249aa93
Longoria.You <longoria.you@gmail.com>
2017-09-07 00:17:02 +0800

    Create DotNet Core Version Solution

---
+++ DotNetCore/SamuriaAppCore/SamuraiAppCore.Data/SamuraiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiAppCore.Domain;
+
+namespace SamuraiAppCore.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Samurai> Samurais { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=WORK; Database=SamuraiDataCore; Trusted_Connection=True;");
+        }
+    }
+}
